18f71d3715cbb4e0d9c4ddf5dac248f93ba977b8
Artem Arsenyev <artem12351235@gmail.com>
2026-01-25 11:56:35 +0100

    init

---
+++ samoprodej/src/test/java/samoprodej/demo/SamoprodejApplicationTests.java
@@ -0,0 +1,13 @@
+package samoprodej.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamoprodejApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
