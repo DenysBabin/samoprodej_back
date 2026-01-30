15e852b616f6013860504f9fb74e4ae072647e3b
Artem Arsenyev <artem12351235@gmail.com>
2026-01-30 14:41:26 +0100

    mod

--- samoprodej/src/main/java/samoprodej/samoprodej/auth/infrastructure/InMemoryUserAccountPort.java
+++ samoprodej/src/main/java/samoprodej/samoprodej/auth/infrastructure/InMemoryUserAccountPort.java
@@ -2,7 +2,6 @@ package samoprodej.samoprodej.auth.infrastructure;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import samoprodej.auth.domain.*;
 import samoprodej.samoprodej.auth.domain.AuthUser;
 import samoprodej.samoprodej.auth.domain.UserAccountPort;
 
