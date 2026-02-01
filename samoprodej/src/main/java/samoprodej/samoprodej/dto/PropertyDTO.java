6e5a7b006ba9e939a5de37f1a66c47576f6bb053
bondk <bond.kyryl@gmail.com>
2026-02-01 15:42:48 +0100

    feat: Property update

---
+++ samoprodej/src/main/java/samoprodej/samoprodej/dto/PropertyDTO.java
@@ -0,0 +1,53 @@
+package samoprodej.samoprodej.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import samoprodej.samoprodej.enums.ParkingType;
+import samoprodej.samoprodej.enums.PropertyType;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+public class PropertyDTO {
+
+    private UUID id;
+
+    @NotNull
+    private UUID ownerUserId;
+
+    @NotNull
+    private PropertyType type;
+
+    private String country = "CZ";
+
+    @NotBlank
+    private String city;
+    private String district;
+    private String street;
+    private String houseNumber;
+    private String addressText;
+
+    private BigDecimal lat;
+    private BigDecimal lng;
+
+    private String dispozice;
+    private Integer roomsCount;
+    private Integer floor;
+    private Integer totalFloors;
+
+    @NotNull
+    private BigDecimal areaM2;
+    private BigDecimal balconyAreaM2;
+    private BigDecimal cellarAreaM2;
+
+    private Boolean hasBalcony;
+    private Boolean hasTerrace;
+    private Boolean hasLoggia;
+    private Boolean hasGarden;
+    private Boolean hasCellar;
+    private Boolean hasElevator;
+
+    private ParkingType parkingType;
+}
---
+++ samoprodej/src/main/java/samoprodej/samoprodej/dto/PropertyMediaDTO.java
@@ -0,0 +1,24 @@
+package samoprodej.samoprodej.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import samoprodej.samoprodej.enums.MediaType;
+
+import java.util.UUID;
+
+@Data
+public class PropertyMediaDTO {
+
+    private UUID id;
+    private UUID propertyId;
+
+    @NotNull
+    private MediaType type;
+
+    @NotBlank
+    private String url;
+
+    private String previewUrl;
+    private Integer sortOrder;
+}
