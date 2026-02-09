74b956769492a20a8f86db55fd3a8a076a0f7e05
bondk <bond.kyryl@gmail.com>
2026-02-09 18:31:19 +0100

    version after tests

--- samoprodej/src/main/java/samoprodej/samoprodej/SamoprodejApplication.java
+++ samoprodej/src/main/java/samoprodej/samoprodej/SamoprodejApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SamoprodejApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SamoprodejApplication.class, args);
 	}
-
 }
